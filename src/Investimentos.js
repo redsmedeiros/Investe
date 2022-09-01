4737819b3010dad70f8ca905c1f011d36cbcab71
Rodolpho medeiros <redsmedeiros@gmail.com>
2022-09-01 19:46:08 +0000

    Update 2 files

--- src/Investimentos.js
+++ src/Investimentos.js
@@ -1,5 +1,16 @@
+import "./Investimentos.css"
 const investimentos = () => {
-  return <div>teste</div>;
+  return <div>
+      <div className="card">
+          <div className="card-titulo">
+              <h1>Carteira</h1>
+              <button>+ Add</button>
+          </div>
+          <div className="card-body">
+              
+          </div>
+      </div>
+  </div>;
 };
 
 export default investimentos;
