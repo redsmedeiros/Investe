87cfbc8f7f85ed7f3998dcc9f6b18612ef5bc4ab
redsmedeiros <91477264+redsmedeiros@users.noreply.github.com>
2022-09-01 14:21:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Investimentos from "./Investimentos";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Investimentos></Investimentos>
+    </div>
+  );
+}
---
+++ src/Investimentos.js
@@ -0,0 +1,5 @@
+const investimentos = () => {
+  return <div></div>;
+};
+
+export default investimentos;
