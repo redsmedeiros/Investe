b94b2ab46134fab4f88b6b1ce2328ce013838eb6
Rodolpho medeiros <redsmedeiros@gmail.com>
2022-09-02 00:42:02 +0000

    Update Investimentos.css and Investimentos.js

--- src/Investimentos.js
+++ src/Investimentos.js
@@ -1,5 +1,12 @@
 import "./Investimentos.css"
 const investimentos = () => {
+    
+    const teste = "teste"
+    
+    const dados = [
+        {nome: "Tesouro Direto"},
+        {nome: "Vale s.a"}
+    ]
   return <div>
       <div className="card">
           <div className="card-titulo">
@@ -7,7 +14,14 @@ const investimentos = () => {
               <button>+ Add</button>
           </div>
           <div className="card-body">
-              
+              <div className="card-conteudo">
+                  {dados.map((dado)=>(
+                      <div className="card-linha">
+                          <h4>{dado.nome}</h4>
+                          <button>+</button>
+                      </div>
+                  ))}
+              </div>
           </div>
       </div>
   </div>;
