1258ff4232ef34871e907a0527f234fd88624175
Rodolpho medeiros <redsmedeiros@gmail.com>
2022-09-03 22:04:42 +0000

    Update 0 files

---
+++ src/Menu.js
@@ -0,0 +1,34 @@
+import "./Menu.css"
+
+const menu = ()=>{
+    return(
+        <div>
+            <div className="navegacao">
+                <ul>
+                    <li className="lista">
+                        <a>
+                            <span className="lista"></span>
+                        </a>
+                    </li>
+                    <li className="lista">
+                        <a>
+                            <span className="lista"></span>
+                        </a>
+                    </li>
+                    <li className="lista">
+                        <a>
+                            <span className="lista"></span>
+                        </a>
+                    </li>
+                    <li className="lista">
+                        <a>
+                            <span className="lista"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default menu
