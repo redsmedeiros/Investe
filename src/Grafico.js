795d1f61f881184117a47fe94d92df3b11b98fcb
Rodolpho medeiros <redsmedeiros@gmail.com>
2022-09-03 20:00:17 +0000

    Update 4 files

--- src/Grafico.js
+++ src/Grafico.js
@@ -3,11 +3,24 @@ import "./Grafico.css"
 const grafico = ()=>{
     
     const investimentos = [
-        {id:1, nome: "Tesouro Direto", qtd:2},
-        {id:2, nome: "Vale", qtd: 2},
-        {id:3, nome: "Petrobrás", qtd: 1}
+        {id:1, nome: "Tesouro Direto", qtd:7, color:"#000080"},
+        {id:2, nome: "Vale", qtd: 4, color:"#ffd700"},
+        {id:3, nome: "Petrobrás", qtd: 1, color:"#00fa9a"}
         
     ]
+    
+    const cor = [
+        "#1c1c1c",
+        "#0000CD",
+        "#FFD700"
+        ]
+    
+    const geradorCor = ()=>{
+        investimentos.forEach(x =>{
+          let number = Math.random()
+          return "teste"
+        })
+    }
     
     const calculoGrafico = ()=>{
         let total = 0
@@ -31,9 +44,16 @@ const grafico = ()=>{
     
     return (
         <div className="container-grafico">
+            <h3>Dados da Carteira</h3>
            {investimentos.map(i => (
-               <div style={{width: height(i.qtd), height: "40px", backgroundColor: "#1c1c1c", }}>
-                   {height(i.qtd)}
+               <div>
+                   <div>
+                       <h5>{i.nome}</h5>
+                       <div className="grafico-dados">
+                          <div style={{backgroundColor: i.color, width: height(i.qtd), height: "40px", borderRadius:"10px", marginRight:"5px"}}></div>
+                          <p>{percent(i.qtd)}</p>
+                       </div>
+                   </div>
                </div>
            ))}
         </div>
